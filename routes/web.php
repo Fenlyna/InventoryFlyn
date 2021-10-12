0d85704b5a1bb04bdc7d4e6964516eb59de243d9
NatsuhimeFenlyna <irham.thehedgehog@gmail.com>
2021-10-12 16:00:09 +0700

    naon weh

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/name/{name}/{nrp}', function ($name,$nrp) {
+    return "Hai " . $name . $nrp;
+})->where('name', '[A-za-z]+');
